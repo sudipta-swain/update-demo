05b973f2849626f61e25038146330f1844c9d758
sudipta-swain <narayanswainjsp9gmail.com>
2023-10-17 19:25:01 +0530

    Add and terminate or delet some lones of code

---
+++ attributes.js
@@ -0,0 +1,8 @@
+//Non primitives data type objects in js
+const item = {
+    "Sudipta": true,
+    "Soumya": false,
+    "Srinibas": 57,
+    "Santosh":undefined
+}
+  console.log(item["Srinibas"])
